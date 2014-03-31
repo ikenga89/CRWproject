44630e5e2c9e7853acc65386592cd43fc6ef110a
ikenga89 <x.guien@ly610108.local>
2014-03-31 14:18:10 +0200

    Formulaire

--- controllers/crw.php
+++ controllers/crw.php
@@ -23,16 +23,43 @@
 		var_dump($posts);
 		die();
 		*/
-		
-		
-		
+
 		return $app['twig']->render('home.twig', array(
 			'posts' => $posts['data'],	
 		));
 		
 
 		//return $app['twig']->render('home.twig');
-
+		$form = $app['form.factory']->createBuilder('form')
+		->add('nom', 'text', array(
+			'label' => 'Nom : ',
+			'constraints' => array(
+			new Assert\NotBlank(), 
+			new Assert\Length(array('min' => 2))
+			)
+		))
+		->add('email', 'email', array(
+			'label' => 'Mail : ',
+			'constraints' => array(
+			new Assert\NotBlank(), 
+			new Assert\Length(array('min' => 2))
+			)
+		))
+		->add('section', 'text', array(
+			'label' => 'Section : ',
+			'constraints' => array(
+			new Assert\NotBlank(), 
+			new Assert\Length(array('min' => 2))
+			)
+		))
+		->add('compose', 'text', array(
+			'label' => 'Composé : ',
+			'constraints' => array(
+			new Assert\NotBlank(), 
+			new Assert\Length(array('min' => 2))
+			)
+		))
+		->getForm();
 
 	})->bind('home');
 	
