a7a57b71abb234450c6ec299a16be5bac629357a
Maxime SIFFLET <m.sifflet@ly610106.local>
2014-03-31 14:28:40 +0200

    Merge branch 'master' of https://github.com/ikenga89/CRWproject
    Conflicts:
    	controllers/crw.php

--- controllers/crw.php
+++ controllers/crw.php
@@ -10,32 +10,61 @@
 
 		$posts = $app['facebook']->api('/110864882309437/posts');
 
-		/*
-		foreach ($posts['data'] as $key => $value) {
-			echo $value['message'].'<br /><br />';
-		}
-		*/
-		
+		$twitter_client = new \Guzzle\Http\Client('https://api.twitter.com/{version}', array(
+                'version' => '1.1'
+            ));
+            $twitter_client->addSubscriber(new \Guzzle\Plugin\Oauth\OauthPlugin(array(
+                'consumer_key'  => 'ARfJFo5NwBMBvQ7MCJRBkQ',
+                'consumer_secret' => 'ixMxmsuHV2LFLNE9QOXex3DcxGBkzX5ucLuKh4K3xWg',
+                'token'       => '2420427098-eK2Fdjo4dwkf3Rc0Huptgv4QeFCjNBBQKbLwBMa',
+                'token_secret'  => 'TXfS7cZW9THyI65vurPnSOp5rEmpnX9eoRi55ryS9cdju'
+            )));
+ 
+            $request = $twitter_client->get('statuses/user_timeline.json');
+            $request->getQuery()->set('count', 5);
+            $request->getQuery()->set('screen_name', 'SciencesULyon');
+            $response = $request->send();
+ 
+            $tweets = json_decode($response->getBody());
+            
+        // Création formulaire
+        //return $app['twig']->render('home.twig');
+		$form = $app['form.factory']->createBuilder('form')
+		->add('nom', 'text', array(
+			'label' => 'Nom : ',
+			'constraints' => array(
+			new Assert\NotBlank(), 
+			new Assert\Length(array('min' => 2))
+			)
+		))
+		->add('email', 'email', array(
+			'label' => 'Mail : ',
+			'constraints' => array(
+			new Assert\NotBlank(), 
+			new Assert\Length(array('min' => 2))
+			)
+		))
+		->add('section', 'text', array(
+			'label' => 'Section : ',
+			'constraints' => array(
+			new Assert\NotBlank(), 
+			new Assert\Length(array('min' => 2))
+			)
+		))
+		->add('compose', 'text', array(
+			'label' => 'Composé : ',
+			'constraints' => array(
+			new Assert\NotBlank(), 
+			new Assert\Length(array('min' => 2))
+			)
+		))
+		->getForm();    
 
-		
-		/*
-		echo '<pre>';
-		var_dump($posts);
-		die();
-		*/
-		
-		
-		
 		return $app['twig']->render('home.twig', array(
-			'posts' => $posts['data'],	
+			'posts' => $posts, 'tweets' => $tweets	
 		));
-		
-
-		//return $app['twig']->render('home.twig');
 
 
 	})->bind('home');
-	
-
 
 	return $blog;
