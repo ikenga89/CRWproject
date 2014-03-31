6985eccfa658a98e1f598ee8984d8cc3a411f7f0
cedricono <c.maiano@ly610109.local>
2014-03-31 14:07:21 +0200

    Tweet
    Tweet

--- controllers/crw.php
+++ controllers/crw.php
@@ -10,27 +10,28 @@
 
 		$posts = $app['facebook']->api('/110864882309437/posts');
 
-		/*
-		foreach ($posts['data'] as $key => $value) {
-			echo $value['message'].'<br /><br />';
-		}
-		*/
-		
+		$twitter_client = new \Guzzle\Http\Client('https://api.twitter.com/{version}', array(
+                'version' => '1.1'
+            ));
+            $twitter_client->addSubscriber(new \Guzzle\Plugin\Oauth\OauthPlugin(array(
+                'consumer_key'  => 'ARfJFo5NwBMBvQ7MCJRBkQ',
+                'consumer_secret' => 'ixMxmsuHV2LFLNE9QOXex3DcxGBkzX5ucLuKh4K3xWg',
+                'token'       => '2420427098-eK2Fdjo4dwkf3Rc0Huptgv4QeFCjNBBQKbLwBMa',
+                'token_secret'  => 'TXfS7cZW9THyI65vurPnSOp5rEmpnX9eoRi55ryS9cdju'
+            )));
+ 
+            $request = $twitter_client->get('statuses/user_timeline.json');
+            $request->getQuery()->set('count', 5);
+            $request->getQuery()->set('screen_name', 'SciencesULyon');
+            $response = $request->send();
+ 
+            $tweets = json_decode($response->getBody());
 
-		
-		/*
-		echo '<pre>';
-		var_dump($posts);
-		die();
-		*/
-		
-		/*
+
+
 		return $app['twig']->render('home.twig', array(
-			'posts' => $posts,	
+			'posts' => $posts, 'tweets' => $tweets	
 		));
-		*/
-
-		return $app['twig']->render('home.twig');
 
 
 	})->bind('home');
