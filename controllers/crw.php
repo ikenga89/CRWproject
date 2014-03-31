f14bf1ff1c6dee82960b212fcbd6e26529e851a4
Maxime SIFFLET <m.sifflet@ly610106.local>
2014-03-31 14:33:22 +0200

    commit

--- controllers/crw.php
+++ controllers/crw.php
@@ -12,20 +12,21 @@
 
 		$twitter_client = new \Guzzle\Http\Client('https://api.twitter.com/{version}', array(
                 'version' => '1.1'
-            ));
-            $twitter_client->addSubscriber(new \Guzzle\Plugin\Oauth\OauthPlugin(array(
-                'consumer_key'  => 'ARfJFo5NwBMBvQ7MCJRBkQ',
-                'consumer_secret' => 'ixMxmsuHV2LFLNE9QOXex3DcxGBkzX5ucLuKh4K3xWg',
-                'token'       => '2420427098-eK2Fdjo4dwkf3Rc0Huptgv4QeFCjNBBQKbLwBMa',
-                'token_secret'  => 'TXfS7cZW9THyI65vurPnSOp5rEmpnX9eoRi55ryS9cdju'
-            )));
+        ));
+        
+        $twitter_client->addSubscriber(new \Guzzle\Plugin\Oauth\OauthPlugin(array(
+            'consumer_key'  => 'ARfJFo5NwBMBvQ7MCJRBkQ',
+            'consumer_secret' => 'ixMxmsuHV2LFLNE9QOXex3DcxGBkzX5ucLuKh4K3xWg',
+            'token'       => '2420427098-eK2Fdjo4dwkf3Rc0Huptgv4QeFCjNBBQKbLwBMa',
+            'token_secret'  => 'TXfS7cZW9THyI65vurPnSOp5rEmpnX9eoRi55ryS9cdju'
+        )));
  
-            $request = $twitter_client->get('statuses/user_timeline.json');
-            $request->getQuery()->set('count', 5);
-            $request->getQuery()->set('screen_name', 'SciencesULyon');
-            $response = $request->send();
- 
-            $tweets = json_decode($response->getBody());
+        $request = $twitter_client->get('statuses/user_timeline.json');
+        $request->getQuery()->set('count', 5);
+        $request->getQuery()->set('screen_name', 'SciencesULyon');
+        $response = $request->send();
+
+        $tweets = json_decode($response->getBody());
             
         // Création formulaire
         //return $app['twig']->render('home.twig');
